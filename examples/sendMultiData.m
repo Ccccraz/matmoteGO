a8399f7a9df68108dd6463fe32d1c0f25113cfc2
HuYang <hy.ccccr@gmail.com>
2025-03-19 18:57:03 +0800

    feat: :sparkles: add example to send data to multiple endpoints

---
+++ examples/sendMultiData.m
@@ -0,0 +1,23 @@
+function sendMultiData()
+
+remote = MatmoteGo();
+
+% create custom endpoint trial1 and trial2
+endpoint_1 = "trial1";
+endpoint_2 = "trial2";
+remote.createEndpoint(endpoint_1);
+remote.createEndpoint(endpoint_2);
+
+% prepare data for three different endpoints
+data = struct('name', 'abc', 'age', 10, 'salary', 50000);
+data_1 = struct('name', 'bca', 'age', 20, 'salary', 50000);
+data_2 = struct('name', 'cba', 'age', 30, 'salary', 50000);
+
+for i = 1:10
+    pause(1);
+    remote.send(data); % send data to default endpoint: data
+    remote.sendTo(data_1, endpoint_1); % send data to trial_001 endpoint
+    remote.sendTo(data_2, endpoint_2); % send data to trial_002 endpoint
+end
+
+end
