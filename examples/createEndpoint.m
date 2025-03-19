2efdacbecd59d5030c631ba2eec8ccbd272414c7
HuYang <hy.ccccr@gmail.com>
2025-03-19 16:11:12 +0800

    chore: :pencil2: add lost ":"

--- examples/createEndpoint.m
+++ examples/createEndpoint.m
@@ -9,6 +9,6 @@ data = struct('name', 'Alice', 'age', 30, 'salary', 50000);
 for i = 1:10
     remote.sendTo(data, endpoint);
     pause(1);
-    disp("Wrote " + num2str(i) + " msg to endpoint " + endpoint);
+    disp("Wrote " + num2str(i) + " msg to endpoint: " + endpoint);
 end
 end
